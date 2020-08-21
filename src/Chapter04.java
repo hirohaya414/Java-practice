3ddbfb82d6ec82806891f282b901e460cf1eff54
hiro <hiro@192.168.10.44>
2020-08-22 02:18:43 +0900

    [Finish] 4章　確認問題

---
+++ src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String str1;
+		str1 = "zoomin";
+		System.out.println(str1);
+
+		str1 = "DMM";
+		System.out.println(str1);
+
+		final int LUCKY_NUMBER = 100;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
