4cc0103bfa74247352e0b00039db659e96c48b37
hiro <hiro@192.168.10.44>
2020-08-22 16:16:50 +0900

    [Finish] 5章　確認問題

---
+++ src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 10000000000L;
+		double d = 3.14;
+		boolean b = true;
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+
+		Integer i = 100;
+		Float f =1.4142f;
+		Character c = 'う';
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(c);
+
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+	}
+}
